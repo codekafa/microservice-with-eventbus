634fbdc60f6914d69571f5a76186cbd93ba346c4
AzureAD\TayfunKoç <tyfnkc@gmail.com>
2024-03-07 14:22:01 +0300

    asd as dsa d

--- Data.Infrastructure/obj/Debug/net7.0/Data.Infrastructure.AssemblyInfo.cs
+++ Data.Infrastructure/obj/Debug/net7.0/Data.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6490ba707877b27d33c869e340f8914cb88787a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Repository.Infrastructure/obj/Debug/net7.0/Repository.Infrastructure.AssemblyInfo.cs
+++ Repository.Infrastructure/obj/Debug/net7.0/Repository.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6490ba707877b27d33c869e340f8914cb88787a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
