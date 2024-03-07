e96337cd164ff4d25b0c5e0265f1d0217fe54e77
AzureAD\TayfunKoç <tyfnkc@gmail.com>
2024-03-07 14:13:06 +0300

    asdasd

--- Domain/Domain.Dto/obj/Debug/net7.0/Domain.Dto.AssemblyInfo.cs
+++ Domain/Domain.Dto/obj/Debug/net7.0/Domain.Dto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain.Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6490ba707877b27d33c869e340f8914cb88787a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain.Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain.Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Web.Api.Gateway/obj/Debug/net7.0/Web.Api.Gateway.AssemblyInfo.cs
+++ Web.Api.Gateway/obj/Debug/net7.0/Web.Api.Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Api.Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6490ba707877b27d33c869e340f8914cb88787a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Api.Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Api.Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
